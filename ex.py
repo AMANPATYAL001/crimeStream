ba25e2e09cdfb90eef4aaa98710e5073af68f4da
Aman Patyal <amanpatyal79@gmail.com>
2021-10-30 16:18:41 +0530

    req.txt

--- ex.py
+++ ex.py
@@ -1,12 +1,11 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import pandas as pd
 import numpy as np
+from textblob import TextBlob
 import re
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
+# import nltk
+# nltk.download('stopwords')
+# from nltk.corpus import stopwords
+# from nltk.stem import SnowballStemmer
 # from keras.preprocessing.text import Tokenizer
 # from keras.preprocessing.sequence import pad_sequences
 import streamlit as st
@@ -17,6 +16,8 @@ import folium
 from wordcloud import WordCloud
 import itertools
 import snscrape.modules.twitter as sntwitter
+
+st.set_page_config(layout="wide")
 # import tensorflow as tf
 
 # model = tf.lite.Interpreter(model_path="res/converted_quant_model.tflite")
@@ -33,35 +34,32 @@ import snscrape.modules.twitter as sntwitter
 # tokenizer = pickle.load(infile)
 # infile.close()
 
-TEXT_CLEANING_RE = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
+# TEXT_CLEANING_RE = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
 
-SEQUENCE_LENGTH = 300
+# SEQUENCE_LENGTH = 300
 
-POSITIVE = "POSITIVE"
-NEGATIVE = "NEGATIVE"
-NEUTRAL = "NEUTRAL"
-SENTIMENT_THRESHOLDS = (0.4, 0.7)
+# POSITIVE = "POSITIVE"
+# NEGATIVE = "NEGATIVE"
+# NEUTRAL = "NEUTRAL"
+# SENTIMENT_THRESHOLDS = (0.4, 0.7)
 
 # loc = '30.88859095134539, 76.68799584206599, 300km'
-inputT=pd.DataFrame(itertools.islice(sntwitter.TwitterSearchScraper(f'near:"Delhi" within:100km ').get_items(),600))     # since:2021-10-25 until:2021-10-26
-print(inputT.content.isna().sum())
-inputT=inputT[inputT.lang=='en']
 
-stop_words = stopwords.words("english")
-stemmer = SnowballStemmer("english")
-def preprocess(text, stem=False):
-    text = re.sub(TEXT_CLEANING_RE, ' ', str(text).lower()).strip()
-    tokens = []
-    for token in text.split():
-        if token not in stop_words:
-            if stem:
-                tokens.append(stemmer.stem(token))
-            else:
-                tokens.append(token)
-    return " ".join(tokens)
-# out=inputT.text.copy()
 
-out=inputT.content.apply(lambda x: preprocess(x))
+# stop_words = stopwords.words("english")
+# stemmer = SnowballStemmer("english")
+# def preprocess(text, stem=False):
+#     text = re.sub(TEXT_CLEANING_RE, ' ', str(text).lower()).strip()
+#     tokens = []
+#     for token in text.split():
+#         if token not in stop_words:
+#             if stem:
+#                 tokens.append(stemmer.stem(token))
+#             else:
+#                 tokens.append(token)
+#     return " ".join(tokens)
+
+# out=inputT.content.apply(lambda x: preprocess(x))
 
 
 # def decode_sentiment(score, include_neutral=True):
@@ -76,7 +74,7 @@ out=inputT.content.apply(lambda x: preprocess(x))
 #     else:
 #         return NEGATIVE if score < 0.5 else POSITIVE
 
-# usr_neg=[]
+usr_neg=[]
 # def predict(text_list, include_neutral=True):
     
 #     # a=time.time()
@@ -99,7 +97,6 @@ out=inputT.content.apply(lambda x: preprocess(x))
 
 # no=np.random.randint(0,190,189)
 
-# @st.cache
 def folium_map():
     df=pd.read_csv('res/part1.csv')[['Police Station','Latitude','Longitude','Event Type']]
     dfP1=df[df['Police Station']=='PS1'].reset_index()
@@ -134,9 +131,14 @@ def folium_map():
     group4.add_to(m)
 
     folium.map.LayerControl('topright', collapsed=False).add_to(m)
-    # return folium_static(m)
-    folium_static(m)
+    return folium_static(m)
+    
 folium_map()
+noTweets=st.slider('No. of tweets',min_value=10,max_value=700,value=200)
+with st.spinner('Wait for it...'):
+    inputT=pd.DataFrame(itertools.islice(sntwitter.TwitterSearchScraper(f'near:"Mumbai" within:100km ').get_items(),noTweets))     # since:2021-10-25 until:2021-10-26
+
+inputT=inputT[inputT.lang=='en']
 
 with st.expander("Word Cloud"):
         # st.header('WordCloud')
@@ -153,26 +155,81 @@ with st.expander("Word Cloud"):
             transformed_wine_mask[i] = list(map(transform_format, tw_mask[i]))
         wc = WordCloud(background_color="white", max_words=1000, mask=transformed_wine_mask,width=300,contour_color='firebrick',contour_width=2)
                     
-        wc.generate(' '.join(out.values))
+        wc.generate(' '.join(inputT.content.values).replace('https',''))
         st.image(wc.to_array(),width=370)
-
-
-
+def getSentiment(text_list):
+    no,positive,neutral,negative=0,0,0,0
+    for j,i in enumerate(text_list):
+        
+        score=TextBlob(i).sentiment.polarity
+        if score < 0:
+            usr_neg.append((j,score))
+            negative+=1
+        elif score == 0:
+            neutral+=1
+        else:
+            positive+=1
+    return positive,neutral,negative
 
 # predict(out.values)
+positive,neutral,negative=getSentiment(inputT.content)
 
-# neg_df=pd.DataFrame(usr_neg, columns =['ind','score'])
-# neg_df.set_index('ind',inplace=True)
+neg_df=pd.DataFrame(usr_neg, columns =['ind','score'])
+neg_df.set_index('ind',inplace=True)
 # 
-# final_df=inputT.join(neg_df,how='left').sort_values('score',ascending=True).dropna(subset=['score'])
+final_df=inputT.join(neg_df,how='left').sort_values('score',ascending=True).dropna(subset=['score'])
 
 
 # print(final_df.content.isna().sum())
-# st.write('Date','Tweet ID','Score','Username','Name','Location')
-# def fun(x):
-#     print(x['date'],x['id'])
-#     st.write(x['date'],round(x['score'],3),x['user']['username'],x['user']['displayname'],x['user']['location'])
-# final_df.apply(fun,axis=1)
+# st.markdown('Date\tTweet ID\tScore\tUsername\tName\tLocation')
+with st.expander("Sentiment Graph"):
+    co1,_=st.columns(2)
+    with co1:
+        st.bar_chart(pd.DataFrame([[positive,neutral,negative]],columns=["POSITIVE", "NEUTRAL", "NEGATIVE"]))
+
+st.warning('This is a warning')
+col1,col2,col3,col4,col5,col6= st.columns([1,1,1,1,3,1])
+with col1:
+    st.subheader("username")
+with col2:
+    st.subheader("Location")
+with col3:
+    st.subheader("Date")
+with col4:
+    st.subheader("ReplyCount")
+with col5:
+    st.subheader("\tURL")
+with col6:
+    st.subheader("Profile Pic")
+
+def fun(x):
+    with col1:
+        st.write(x['user']['username'])
+    with col2:
+        try:
+            st.write(x['user']['location'])
+        except:
+            st.write('NO location')
+    with col3:
+        st.write(x['date'])
+    with col4:
+        st.write(x['replyCount'])
+    with col5:
+        st.write(x['url'])
+    with col6:
+        try:
+            st.image(x['user']['profileImageUrl'])
+        except:
+            st.write('NO Image')
+    # st.image(x['user']['profileImageUrl'])
+    # st.write(x['date'],x['user']['username'],x['user']['displayname'],)
+final_df.apply(fun,axis=1)
+
+
+
+
+
+
 # for i in inputT.iloc[usr_neg]:
     # 
 # tweet = st.text_input('Movie title', 'hey i am eating pizza.Come and join me.')
